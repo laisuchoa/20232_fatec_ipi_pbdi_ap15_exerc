70ce8147bb54207f032e1c3ff75a9b65583a6847
Ronaldo Souza Chaves <ronaldosouzachaves@outlook.com>
2023-11-06 19:10:40 -0300

    feat(ap15): adiciona exercício 1.1

--- script.sql
+++ script.sql
@@ -2,3 +2,4 @@
 
 CREATE EXTENSION IF NOT EXISTS tablefunc;
 
+SELECT * FROM crosstab ('SELECT location, month, AVG(raindays)::int FROM rainfalls GROUP BY location, month ORDER BY location, month;') AS tab_ref_cruzada ("local" TEXT, "jan" INT, "fev" INT, "mar" INT, "abr" INT, "mai" INT, "jun" INT, "jul" INT, "ago" INT, "set" INT, "out" INT, "nov" INT, "dez" INT)
